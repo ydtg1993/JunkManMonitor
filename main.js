784a48ff1d498ac35b02ad7c0ca113daea2d231e
ydtg1993 <946818508@qq.com>
2019-04-03 13:37:40 +0800

    svg update ipc

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const IpcMain = electron.ipcMain;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 1400,
-    height: 900,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+    width: 1200,
+    height: 800,
+    show:false,
+    frame: false
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -26,26 +28,28 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow = null;
     socketWorker.close();
-  })
+  });
 
-  const { ipcMain } = require('electron')
-  ipcMain.on('socket-event', (event, arg) => {
-    console.log(arg) // prints "ping"
+  IpcMain.on('socket-event', (event, arg) => {
     if(arg == 'connect'){
       socketWorker.connect();
       event.sender.send('asynchronous-reply', 'pong')
     }else if(arg == 'disconnect'){
       socketWorker.close();
     }
-  })
+  });
+
+  mainWindow.on('ready-to-show',()=>{
+    mainWindow.show();
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -54,7 +58,7 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
 
 app.on('activate', function () {
   // On macOS it's common to re-create a window in the app when the
@@ -62,31 +66,39 @@ app.on('activate', function () {
   if (mainWindow === null) {
     createWindow()
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 let socketWorker = {
   connect:function () {
     let net = require('net');
-    let HOST = '103.46.128.49';
+    let HOST = '47.75.133.2141';
     let PORT = 26841;
 
-    const JunkManClient = new net.Socket(({
+    global.JunkManClient = new net.Socket(({
       readable:true,
       writable:true,
     }));
 
-    global.JunkManClient = JunkManClient;
+    JunkManClient.setTimeout(3000);
+    JunkManClient.on('error', () => {
+      global.JunkManClient = null;
+      electron.dialog.showMessageBox({
+        title  : 'connect error',
+        type:'info',
+        message : 'please check your connection config!'
+      })
+    });
 
     JunkManClient.connect(PORT, HOST, function() {
       JunkManClient.write(`{"agent":"client","status":"start"}`)
     });
   },
   close:function () {
-    if(typeof JunkManClient != null) {
-      JunkManClient.write(`{"agent":"client","status":"end"}`);
+    if(global.JunkManClient != null) {
+      JunkManClient.end(`{"agent":"client","status":"end"}`);
       global.JunkManClient = null;
     }
   }
-}
+};
